d0a0710280da16a45dcf29527cd2a04c79a2f05e
aleskruba <74367962+aleskruba@users.noreply.github.com>
2022-11-30 10:40:24 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './features/todos/TodoList';
+import React from 'react';
+
+
+function App() {
+  
+  return (
+ 
+  <TodoList/>
+
+    )
+  
+
+}
+
+export default App;
